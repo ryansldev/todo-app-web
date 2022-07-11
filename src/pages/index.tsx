31df7881be180603aad90a16ec2b8886338384c3
ryansldev <ryanlima1309@gmail.com>
2022-07-11 16:57:39 -0300

    feat: start initial page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,65 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
-import { Checkbox, CheckboxGroup, Container, StackDivider, Heading, Stack, Box, Text } from '@chakra-ui/react'
+import {
+  Checkbox,
+  Container,
+  StackDivider,
+  Heading,
+  Stack,
+  Text,
+  useToast,
+  Button
+} from '@chakra-ui/react'
+import { getTasks, Task, updateTask } from '~services/task.request';
+import { useEffect, useState } from 'react';
+import { EditIcon } from '@chakra-ui/icons';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
+  const toast = useToast();
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const handleGetTasks = async () => {
+    try {
+      const { data: tasks } = await getTasks();
+      setTasks(tasks);
+    } catch (err: any) {
+      toast({
+        title: "Erro ao carregar tarefas",
+        description: err?.message,
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      })
+    }
+  }
+
+  useEffect(() => {
+    handleGetTasks();
+  }, []);
+
+  const handleTask = (id: string, data: { title?: string; description?: string; done?: boolean }) => {
+    const request = async () => {
+      try {
+        await updateTask(id, data)
+        handleGetTasks();
+      } catch (err: any) {
+        toast({
+          title: "Erro ao atualizar a tarefa",
+          description: err?.message,
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+        })
+      }
+    }
+
+    request()
+  }
+
   return (
-    <div>
+    <>
       <Head>
         <title>Todo app</title>
         <meta name="description" content="Todo app" />
@@ -16,30 +70,38 @@ const Home: NextPage = () => {
         <Container>
           <Stack minHeight='calc(100vh)' justifyContent='center' spacing={6}>
             <Heading>Tarefas</Heading>
-            <CheckboxGroup colorScheme='blue' defaultValue={['naruto', 'kakashi']}>
-              <Stack
-                divider={<StackDivider borderColor='gray.200' />}
-                spacing={4}
-                align='stretch'
-              >
-                <Stack spacing={4}>
-                  <Checkbox value='naruto'>Naruto</Checkbox>
-                  <Text>Descrição da tarefa</Text>
-                </Stack>
-                <Stack spacing={4}>
-                  <Checkbox value='sasuke'>Sasuke</Checkbox>
-                  <Text>Descrição da tarefa</Text>
-                </Stack>
-                <Stack spacing={4}>
-                  <Checkbox value='kakashi'>Kakashi</Checkbox>
-                  <Text>Descrição da tarefa</Text>
-                </Stack>
-              </Stack>
-            </CheckboxGroup>
+            <Stack
+              divider={<StackDivider borderColor='gray.200' />}
+              spacing={4}
+              align='stretch'
+            >
+              {tasks && tasks.length > 0
+              ? (
+                tasks.map((task) => (
+                  <Stack key={task.id} justifyContent='space-between' alignItems='flex-start' spacing={6}>
+                    <Stack spacing={2}>
+                      <Checkbox
+                        isChecked={task.done}
+                        onChange={() => handleTask(task.id, { done: !task.done })}
+                      >
+                        {task.title}
+                      </Checkbox>
+                      <Text fontSize='sm'>{task.description}</Text>
+                    </Stack>
+
+                    <Link href={`/tasks/${task.id}`} passHref>
+                      <Button size='sm' leftIcon={<EditIcon />}>Editar tarefa</Button>
+                    </Link>
+                  </Stack>
+                ))
+              ) : (
+                <Text>Nenhuma tarefa encontrada</Text>
+              )}
+            </Stack>
           </Stack>
         </Container>
       </main>
-    </div>
+    </>
   )
 }
 
