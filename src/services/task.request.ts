5d292732748059fe600816baf092bc8f8f07026b
ryansldev <ryanlima1309@gmail.com>
2022-07-11 16:57:26 -0300

    feat: start task.request.ts

---
+++ src/services/task.request.ts
@@ -0,0 +1,18 @@
+import { api } from "./api.request";
+
+export type Task = {
+  id: string;
+  title: string;
+  description?: string;
+  done: boolean;
+}
+
+const getTasks = async () => await api.get<Task[]>("/tasks");
+
+const updateTask = async (id: string, body: {
+  title?: string;
+  description?: string;
+  done?: boolean;
+}) => await api.put<Task[]>(`/tasks/${id}`, body);
+
+export { getTasks, updateTask };
